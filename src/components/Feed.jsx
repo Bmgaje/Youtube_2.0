4b3d4da97b7b9768a5a51d052d09ef7c07e72815
Bmgaje <brijeshgajera4205@gmail.com>
2024-02-08 00:14:04 -0500

    project is done

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,4 @@
 import React, { useContext, useEffect } from "react";
-
 import { Context } from "../context/contextApi";
 import LeftNav from "./LeftNav";
 import VideoCard from "./VideoCard";
@@ -17,8 +16,9 @@ const Feed = () => {
       <div className="grow w-[calc(100%-240px)] h-full overflow-y-auto bg-black">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5">
           {!loading &&
-            searchResults.map((item) => {
-              if (item.type !== "video") return false;
+            Array.isArray(searchResults) && // Check if searchResults is an array
+            searchResults?.map((item) => {
+              if (item?.type !== "video") return false;
               return (
                 <VideoCard key={item?.video?.videoId} video={item?.video} />
               );
--- src/shared/VideoLength.jsx
+++ src/shared/VideoLength.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import moment from "moment";
 
-const VideoLength = () => {
-  return <div></div>;
+const VideoLength = ({ time }) => {
+  const duration = moment.duration(time, "seconds");
+  const videoLengthFormatted = moment
+    .utc(duration.asMilliseconds())
+    .format("H:mm:ss");
+
+  return (
+    <div className="absolute bottom-2 right-2 bg-black py-1 px-2 text-white text-xs rounded-md">
+      {videoLengthFormatted}
+    </div>
+  );
 };
 
 export default VideoLength;
